a83cdb6cafeb676e43f045bf220b4b2dbe0413b2
Rahul Rangnekar <rrangnekar@berkeley.edu>
2016-07-06 20:24:19 -0700

    added images, icons, CSS stylesheet, Javascript scriptsheet

---
+++ webscript.js
@@ -0,0 +1,458 @@
+// JavaScript Document
+
+/* RR IMAGE CODE */
+
+function hoverRR() {
+	var image = document.getElementById("RR-img");
+	image.src = "images/RR2.png";
+}
+
+function offRR() {
+	var image = document.getElementById("RR-img");
+	image.src = "images/RR.png";
+}
+
+/* "PERSONAL" SECTION CODE */
+
+/* PI SIGMA EPSILON */
+
+var trackPSE = 'o';
+
+function hoverPSE() {
+	var image = document.getElementById('PSE-pic');
+	if (trackPSE == 'o') {
+		image.src = "images/PSE-hover.jpg";
+		trackPSE = 'h';
+	}
+}
+
+function clickPSE() {
+	var image = document.getElementById("PSE-pic");
+	if (trackPSE == 'd') {
+		image.src = "images/PSE.jpg";
+		trackPSE = 'o';
+	} else {
+		image.src = "images/PSE-desc.jpg";
+		trackPSE = 'd';
+	}
+}
+
+function offPSE() {
+	var image = document.getElementById("PSE-pic");
+	if (trackPSE == 'h' || trackPSE == 'o') {
+		image.src = "images/PSE.jpg";
+		trackPSE = 'o';
+	} else {
+		image.src = "images/PSE-desc.jpg";
+		trackPSE = 'd';
+	}
+}
+
+/* ASSOCIATED STUDENTS OF THE UNIVERSITY OF CALIFORNIA */
+
+var trackASUC = 'o';
+
+function hoverASUC() {
+	var image = document.getElementById('ASUC-pic');
+	if (trackASUC == 'o') {
+		image.src = "images/ASUC-hover.jpg";
+		trackASUC = 'h';
+	}
+}
+
+function clickASUC() {
+	var image = document.getElementById("ASUC-pic");
+	if (trackASUC == 'd') {
+		image.src = "images/ASUC.jpg";
+		trackASUC = 'o';
+	} else {
+		image.src = "images/ASUC-desc.jpg";
+		trackASUC = 'd';
+	}
+}
+
+function offASUC() {
+	var image = document.getElementById("ASUC-pic");
+	if (trackASUC == 'h' || trackASUC == 'o') {
+		image.src = "images/ASUC.jpg";
+		trackASUC = 'o';
+	} else {
+		image.src = "images/ASUC-desc.jpg";
+		trackASUC = 'd';
+	}
+}
+
+/* LAASYA 2016 */
+
+var trackLaasya = 'o';
+
+function hoverLaasya() {
+	var image = document.getElementById('Laasya-pic');
+	if (trackLaasya == 'o') {
+		image.src = "images/Laasya-hover.jpg";
+		trackLaasya = 'h';
+	}
+}
+
+function clickLaasya() {
+	var image = document.getElementById("Laasya-pic");
+	if (trackLaasya == 'd') {
+		image.src = "images/Laasya.jpg";
+		trackLaasya = 'o';
+	} else {
+		image.src = "images/Laasya-desc.jpg";
+		trackLaasya = 'd';
+	}
+}
+
+function offLaasya() {
+	var image = document.getElementById("Laasya-pic");
+	if (trackLaasya == 'h' || trackLaasya == 'o') {
+		image.src = "images/Laasya.jpg";
+		trackLaasya = 'o';
+	} else {
+		image.src = "images/Laasya-desc.jpg";
+		trackLaasya = 'd';
+	}
+}
+
+/* AMERICAN RED CROSS AT CAL */
+
+var trackARC = 'o';
+
+function hoverARC() {
+	var image = document.getElementById('ARC-pic');
+	if (trackARC == 'o') {
+		image.src = "images/ARC-hover.jpg";
+		trackARC = 'h';
+	}
+}
+
+function clickARC() {
+	var image = document.getElementById("ARC-pic");
+	if (trackARC == 'd') {
+		image.src = "images/ARC.jpg";
+		trackARC = 'o';
+	} else {
+		image.src = "images/ARC-desc.jpg";
+		trackARC = 'd';
+	}
+}
+
+function offARC() {
+	var image = document.getElementById("ARC-pic");
+	if (trackARC == 'h' || trackARC == 'o') {
+		image.src = "images/ARC.jpg";
+		trackARC = 'o';
+	} else {
+		image.src = "images/ARC-desc.jpg";
+		trackARC = 'd';
+	}
+}
+
+
+
+
+/* "HOBBIES" SECTION CODE */
+
+/* EVERCISE */
+
+var trackExercise = 'o';
+
+function hoverExercise() {
+	var image = document.getElementById('Exercise-pic');
+	if (trackExercise == 'o') {
+		image.src = "images/Exercise-hover.jpg";
+		trackExercise = 'h';
+	}
+}
+
+function clickExercise() {
+	var image = document.getElementById("Exercise-pic");
+	if (trackExercise == 'd') {
+		image.src = "images/Exercise.jpg";
+		trackExercise = 'o';
+	} else {
+		image.src = "images/Exercise-desc.jpg";
+		trackExercise = 'd';
+	}
+}
+
+function offExercise() {
+	var image = document.getElementById("Exercise-pic");
+	if (trackExercise == 'h' || trackExercise == 'o') {
+		image.src = "images/Exercise.jpg";
+		trackExercise = 'o';
+	} else {
+		image.src = "images/Exercise-desc.jpg";
+		trackExercise = 'd';
+	}
+}
+
+/* COOK */
+
+var trackCook = 'o';
+
+function hoverCook() {
+	var image = document.getElementById('Cook-pic');
+	if (trackCook == 'o') {
+		image.src = "images/Cook-hover.jpg";
+		trackCook = 'h';
+	}
+}
+
+function clickCook() {
+	var image = document.getElementById("Cook-pic");
+	if (trackCook == 'd') {
+		image.src = "images/Cook.jpg";
+		trackCook = 'o';
+	} else {
+		image.src = "images/Cook-desc.jpg";
+		trackCook = 'd';
+	}
+}
+
+function offCook() {
+	var image = document.getElementById("Cook-pic");
+	if (trackCook == 'h' || trackCook == 'o') {
+		image.src = "images/Cook.jpg";
+		trackCook = 'o';
+	} else {
+		image.src = "images/Cook-desc.jpg";
+		trackCook = 'd';
+	}
+}
+
+/* NBA */
+
+var trackNBA = 'o';
+
+function hoverNBA() {
+	var image = document.getElementById('NBA-pic');
+	if (trackNBA == 'o') {
+		image.src = "images/NBA-hover.jpg";
+		trackNBA = 'h';
+	}
+}
+
+function clickNBA() {
+	var image = document.getElementById("NBA-pic");
+	if (trackNBA == 'd') {
+		image.src = "images/NBA.jpg";
+		trackNBA = 'o';
+	} else {
+		image.src = "images/NBA-desc.jpg";
+		trackNBA = 'd';
+	}
+}
+
+function offNBA() {
+	var image = document.getElementById("NBA-pic");
+	if (trackNBA == 'h' || trackNBA == 'o') {
+		image.src = "images/NBA.jpg";
+		trackNBA = 'o';
+	} else {
+		image.src = "images/NBA-desc.jpg";
+		trackNBA = 'd';
+	}
+}
+
+/* CODE */
+
+var trackCode = 'o';
+
+function hoverCode() {
+	var image = document.getElementById('Code-pic');
+	if (trackCode == 'o') {
+		image.src = "images/Code-hover.jpg";
+		trackCode = 'h';
+	}
+}
+
+function clickCode() {
+	var image = document.getElementById("Code-pic");
+	if (trackCode == 'd') {
+		image.src = "images/Code.jpg";
+		trackCode = 'o';
+	} else {
+		image.src = "images/Code-desc.jpg";
+		trackCode = 'd';
+	}
+}
+
+function offCode() {
+	var image = document.getElementById("Code-pic");
+	if (trackCode == 'h' || trackCode == 'o') {
+		image.src = "images/Code.jpg";
+		trackCode = 'o';
+	} else {
+		image.src = "images/Code-desc.jpg";
+		trackCode = 'd';
+	}
+}
+
+/* "PROFESSIONAL" SECTION CODE */
+
+/* STEPPING STONE TALENT RECRUITING */
+
+var trackSS = 'o';
+
+function hoverSS() {
+	var image = document.getElementById('SS-pic');
+	if (trackSS == 'o') {
+		image.src = "images/SteppingStone-hover.jpg";
+		trackSS = 'h';
+	}
+}
+
+function clickSS() {
+	var image = document.getElementById("SS-pic");
+	if (trackSS == 'd') {
+		image.src = "images/SteppingStone.jpg";
+		trackSS = 'o';
+	} else {
+		image.src = "images/SteppingStone-desc.jpg";
+		trackSS = 'd';
+	}
+}
+
+function offSS() {
+	var image = document.getElementById("SS-pic");
+	if (trackSS == 'h' || trackSS == 'o') {
+		image.src = "images/SteppingStone.jpg";
+		trackSS = 'o';
+	} else {
+		image.src = "images/SteppingStone-desc.jpg";
+		trackSS = 'd';
+	}
+}
+
+/* EMBER TECHNOLOGIES */
+
+var trackEmber = 'o';
+
+function hoverEmber() {
+	var image = document.getElementById('Ember-pic');
+	if (trackEmber == 'o') {
+		image.src = "images/Ember-hover.jpg";
+		trackEmber = 'h';
+	}
+}
+
+function clickEmber() {
+	var image = document.getElementById("Ember-pic");
+	if (trackEmber == 'd') {
+		image.src = "images/Ember.jpg";
+		trackEmber = 'o';
+	} else {
+		image.src = "images/Ember-desc.jpg";
+		trackEmber = 'd';
+	}
+}
+
+function offEmber() {
+	var image = document.getElementById("Ember-pic");
+	if (trackEmber == 'h' || trackEmber == 'o') {
+		image.src = "images/Ember.jpg";
+		trackEmber = 'o';
+	} else {
+		image.src = "images/Ember-desc.jpg";
+		trackEmber = 'd';
+	}
+}
+
+/* FULLDIVE VIRTUAL REALITY */
+
+var trackFD = 'o';
+
+function hoverFD() {
+	var image = document.getElementById('FD-pic');
+	if (trackFD == 'o') {
+		image.src = "images/FullDive-hover.jpg";
+		trackFD = 'h';
+	}
+}
+
+function clickFD() {
+	var image = document.getElementById("FD-pic");
+	if (trackFD == 'd') {
+		image.src = "images/FullDive.jpg";
+		trackFD = 'o';
+	} else {
+		image.src = "images/FullDive-desc.jpg";
+		trackFD = 'd';
+	}
+}
+
+function offFD() {
+	var image = document.getElementById("FD-pic");
+	if (trackFD == 'h' || trackFD == 'o') {
+		image.src = "images/FullDive.jpg";
+		trackFD = 'o';
+	} else {
+		image.src = "images/FullDive-desc.jpg";
+		trackFD = 'd';
+	}
+}
+
+/* ICON CODE */
+
+/* FACEBOOK */
+
+function hoverFB() {
+	var image = document.getElementById("fb-icon");
+	image.src = "icons/facebook-color.png";
+}
+
+function offFB() {
+	var image = document.getElementById("fb-icon");
+	image.src = "icons/facebook.png";
+}
+
+/* TWITTER */
+
+function hoverTW() {
+	var image = document.getElementById("tw-icon");
+	image.src = "icons/twitter-color.png";
+}
+
+function offTW() {
+	var image = document.getElementById("tw-icon");
+	image.src = "icons/twitter.png";
+}
+
+/* INSTAGRAM */
+
+function hoverIG() {
+	var image = document.getElementById("ig-icon");
+	image.src = "icons/instagram-color.png";
+}
+
+function offIG() {
+	var image = document.getElementById("ig-icon");
+	image.src = "icons/instagram.png";
+}
+
+/* YELP */
+
+function hoverYP() {
+	var image = document.getElementById("yp-icon");
+	image.src = "icons/yelp-color.png";
+}
+
+function offYP() {
+	var image = document.getElementById("yp-icon");
+	image.src = "icons/yelp.png";
+}
+
+/* GITHUB */
+
+function hoverGH() {
+	var image = document.getElementById("gh-icon");
+	image.src = "icons/github-color.png";
+}
+
+function offGH() {
+	var image = document.getElementById("gh-icon");
+	image.src = "icons/github.png";
+}
