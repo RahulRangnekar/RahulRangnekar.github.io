6517236d9488f43a2a71f9608fc2cc922056086a
Rahul Rangnekar <rrangnekar@berkeley.edu>
2016-08-07 15:54:43 -0700

    revamped the entire website, added landing cover photos for every page, next step is to develop content for each page

---
+++ script.js
@@ -0,0 +1,37 @@
+// Javascript Document
+
+/* RR IMAGE CODE */
+
+function hoverRR() {
+	var image = document.getElementById("RR-img");
+	image.src = "images/RR2.png";
+}
+
+function offRR() {
+	var image = document.getElementById("RR-img");
+	image.src = "images/RR.png";
+}
+
+/* Intro-Pers */
+
+function onPers() {
+	var image = document.getElementById("intro-pers");
+	image.src = "images/intro-pers_hover.jpg";
+}
+
+function offPers() {
+	var image = document.getElementById("intro-pers");
+	image.src = "images/intro-pers.jpg";
+}
+
+/* Intro-Prof */
+
+function onProf() {
+	var image = document.getElementById("intro-prof");
+	image.src = "images/intro-prof_hover.jpg";
+}
+
+function offProf() {
+	var image = document.getElementById("intro-prof");
+	image.src = "images/intro-prof.jpg";
+}
