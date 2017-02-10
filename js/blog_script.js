785d07d8de1114134513cbf9b0907c0480e61e3e
Rahul Rangnekar <rrangnekar@berkeley.edu>
2017-02-10 10:59:02 -0800

    added hover images on blog page

---
+++ js/blog_script.js
@@ -0,0 +1,99 @@
+// JavaScript Document
+
+function hoverApple() {	
+	"use strict";
+	var image = document.getElementById("post-apple");
+	image.src = "images/blog/apple_innovation_hover.jpg";
+}
+
+function offApple() {
+	"use strict";
+	var image = document.getElementById("post-apple");
+	image.src = "images/blog/apple_innovation.jpg";
+}
+
+function hoverFitbit() {	
+	"use strict";
+	var image = document.getElementById("post-fitbit");
+	image.src = "images/blog/fitbit_hover.jpg";
+}
+
+function offFitbit() {
+	"use strict";
+	var image = document.getElementById("post-fitbit");
+	image.src = "images/blog/fitbit.jpg";
+}
+
+function hoverTwitter() {
+	"use strict";
+	var image = document.getElementById("post-twitter");
+	image.src = "images/blog/twitter_vine_hover.jpg";
+}
+
+function offTwitter() {
+	"use strict";
+	var image = document.getElementById("post-twitter");
+	image.src = "images/blog/twitter_vine.jpg";
+}
+
+function hoverGoogle() {	
+	"use strict";
+	var image = document.getElementById("post-google");
+	image.src = "images/blog/google_trips_hover.jpg";
+}
+
+function offGoogle() {
+	"use strict";
+	var image = document.getElementById("post-google");
+	image.src = "images/blog/google_trips.jpg";
+}
+
+function hoverNBA() {	
+	"use strict";
+	var image = document.getElementById("post-nba");
+	image.src = "images/blog/nba_ads_hover.jpg";
+}
+
+function offNBA() {
+	"use strict";
+	var image = document.getElementById("post-nba");
+	image.src = "images/blog/nba_ads.jpg";
+}
+
+function hoverSoup() {	
+	"use strict";
+	var image = document.getElementById("post-soup");
+	image.src = "images/blog/marketing_soup_hover.jpg";
+}
+
+function offSoup() {
+	"use strict";
+	var image = document.getElementById("post-soup");
+	image.src = "images/blog/marketing_soup.jpg";
+}
+
+function hoverJobHunt() {	
+	"use strict";
+	var image = document.getElementById("post-job-hunt");
+	image.src = "images/blog/jobs_hover.jpg";
+}
+
+function offJobHunt() {
+	"use strict";
+	var image = document.getElementById("post-job-hunt");
+	image.src = "images/blog/jobs.jpg";
+}
+
+function hoverKanye() {	
+	"use strict";
+	var image = document.getElementById("post-kanye");
+	image.src = "images/blog/kanye_west_hover.jpg";
+}
+
+function offKanye() {
+	"use strict";
+	var image = document.getElementById("post-kanye");
+	image.src = "images/blog/kanye_west.jpg";
+}
+
+
