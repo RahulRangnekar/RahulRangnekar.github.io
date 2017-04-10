447558c2613b2e8bb1c83d479d91e145a757e075
Rahul Rangnekar <bear@Rahuls-MacBook-Pro.local>
2017-04-10 10:16:31 -0700

    made some changes to blog.html, re-organized css/ and js/

--- js/index_script.js
+++ js/index_script.js
@@ -1,73 +1,49 @@
 // JavaScript Document
 
-function hoverSS() {	
+function hoverFears() {	
 	"use strict";
-	var image = document.getElementById("work-steppingstone");
-	image.src = "images/work/stepping_stone_hover.jpg";
+	var image = document.getElementById("proj-fears");
+	image.src = "images/work/underarmour_hover.jpg";
 }
 
-function offSS() {
+function offFears() {
 	"use strict";
-	var image = document.getElementById("work-steppingstone");
-	image.src = "images/work/stepping_stone.jpg";
-}
-
-function hoverEdisons() {	
-	"use strict";
-	var image = document.getElementById("work-edisons");
-	image.src = "images/work/edisons_hover.jpg";
-}
-
-function offEdisons() {
-	"use strict";
-	var image = document.getElementById("work-edisons");
-	image.src = "images/work/edisons.jpg";
-}
-
-function hoverFulldive() {	
-	"use strict";
-	var image = document.getElementById("work-fulldive");
-	image.src = "images/work/fulldive_hover.jpg";
-}
-
-function offFulldive() {
-	"use strict";
-	var image = document.getElementById("work-fulldive");
-	image.src = "images/work/fulldive.jpg";
+	var image = document.getElementById("proj-fears");
+	image.src = "images/work/underarmour.jpg";
 }
 
 function hoverUA() {	
 	"use strict";
-	var image = document.getElementById("work-underarmour");
+	var image = document.getElementById("proj-underarmour");
 	image.src = "images/work/underarmour_hover.jpg";
 }
 
 function offUA() {
 	"use strict";
-	var image = document.getElementById("work-underarmour");
+	var image = document.getElementById("proj-underarmour");
 	image.src = "images/work/underarmour.jpg";
 }
 
 function hoverGoogle() {	
 	"use strict";
-	var image = document.getElementById("work-google");
+	var image = document.getElementById("proj-DMT");
 	image.src = "images/work/google_hover.jpg";
 }
 
 function offGoogle() {
 	"use strict";
-	var image = document.getElementById("work-google");
+	var image = document.getElementById("proj-DMT");
 	image.src = "images/work/google.jpg";
 }
 
 function hoverLaasya() {	
 	"use strict";
-	var image = document.getElementById("work-laasya");
+	var image = document.getElementById("proj-laasya");
 	image.src = "images/work/laasya_hover.jpg";
 }
 
 function offLaasya() {
 	"use strict";
-	var image = document.getElementById("work-laasya");
+	var image = document.getElementById("proj-laasya");
 	image.src = "images/work/laasya.jpg";
 }
