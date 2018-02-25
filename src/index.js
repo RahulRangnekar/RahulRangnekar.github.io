1b181bbbef7e0c64a6dfdfd4874f2f2338717876
Rahul Rangnekar <rrangnekar96@gmail.com>
2018-02-24 17:51:44 -0800

    added code to keep website dynos from Heroku alive

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let http = require("http");
+setInterval(function() {
+  http.get("https://www.rahrang.xyz");
+}, 20000);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
